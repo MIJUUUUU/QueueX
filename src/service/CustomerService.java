93063fccf5f763d6243f213cc66115fe84d044ef
js_song <sjsong03036@gmal.com>
2026-04-07 14:44:46 +0900

    CustomerDTO add

--- src/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-//실제 기능 구현
-// (고객 흐름 처리 ex. 대기 등록, 조회)
-public class CustomerService {
-    
-}
--- src/service/RecommendationService.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public class RecommendationService {
-    //추천 알고리즘
-}
