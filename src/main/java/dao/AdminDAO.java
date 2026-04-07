fc76d3fbcb9a82740e757bcc658278d962136031
studioym-bb <studioym.bb@gmail.com>
2026-04-07 17:28:06 +0900

    feature/admin-login-service hash value add

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -2,32 +2,34 @@ package dao;
 
 import common.DBUtil;
 import dto.Admin;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminDAO {
-  // 관리자/대기/통계 관련 SQL
-  public Admin findAdmin(String adminAuthCode) {
-    String sql = "SELECT admin_id, admin_auth_code FROM admin WHERE admin_auth_code = ?";
 
-    try {
-      Connection conn = DBUtil.getConnection();
-      PreparedStatement pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, adminAuthCode);
-      ResultSet rs = pstmt.executeQuery();
-      if (rs.next()) {
-        return new Admin(
+  public List<Admin> findAllAdmins() {
+    String sql = "SELECT admin_id, admin_auth_code FROM admin";
+    List<Admin> admins = new ArrayList<>();
+
+    try (
+        Connection conn = DBUtil.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery()
+    ) {
+      while (rs.next()) {
+        admins.add(new Admin(
             rs.getInt("admin_id"),
             rs.getString("admin_auth_code")
-        );
+        ));
       }
-
-
     } catch (Exception e) {
       e.printStackTrace();
-
     }
-    return null;
+
+    return admins;
   }
 }
