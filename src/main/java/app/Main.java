8121979b268bd38e3a264f78ebbdb4aa18145b2c
MIJUUUUU <dlalwnjenny@naver.com>
2026-04-07 13:46:28 +0900

    컴파일 .class 위치 변경

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,18 @@
-package src.main.java.app;
+package app;
+
+import common.DBUtil;
+
+import java.sql.Connection;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("QueueX application started.");
+
+        try (Connection connection = DBUtil.getConnection()) {
+            System.out.println("DB 연결 성공!");
+        } catch (Exception e) {
+            System.out.println("DB 연결 실패");
+            e.printStackTrace();
+        }
+    }
 }
