ae06b3fcc1f0b31e112bdae6b23c96b8826bd80e
studioym-bb <studioym.bb@gmail.com>
2026-04-07 16:07:20 +0900

    refactor

--- src/main/java/dto/Customer.java
+++ src/main/java/dto/Customer.java
@@ -2,28 +2,6 @@ package dto;
 
 //객체
 public class Customer {
-<<<<<<< HEAD
-    private int customerId;
-    private String phone;
-    private String password;
-
-    public Customer() {}
-
-    public Customer(int customerId, String phone, String password) {
-        this.customerId = customerId;
-        this.phone = phone;
-        this.password = password;
-    }
-
-    public int getCustomerId() { return customerId; }
-    public void setCustomerId(int customerId) { this.customerId = customerId; }
-
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-=======
   private int customerId;
   private String phone;
   private String password;
@@ -44,5 +22,4 @@ public class Customer {
 
   public String getPassword() { return password; }
   public void setPassword(String password) { this.password = password; }
->>>>>>> 9e87d33 (refactor Customer)
 }
