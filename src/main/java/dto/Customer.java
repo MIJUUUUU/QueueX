2760956cb8de73e82f111033ede4fbabc8b91b2a
MIJUUUUU <dlalwnjenny@naver.com>
2026-04-07 15:49:57 +0900

    fix: 코드 수정

--- src/main/java/dto/Customer.java
+++ src/main/java/dto/Customer.java
@@ -2,5 +2,24 @@ package dto;
 
 //객체
 public class Customer {
-    
+    private int customerId;
+    private String phone;
+    private String password;
+
+    public Customer() {}
+
+    public Customer(int customerId, String phone, String password) {
+        this.customerId = customerId;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public int getCustomerId() { return customerId; }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
